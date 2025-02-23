4dd002da31c3f7c0c4bc6625d5832b06b39d68ba
Augustine Aguworumba <augustine.aguworumba@gmail.com>
2025-02-22 16:56:52 -0800

    Enters and prints new strings

--- main.cpp
+++ main.cpp
@@ -3,6 +3,12 @@
 using namespace std;
 
 int main(){
-    cout << "Default Template" << endl;
+    string str{};
+    cout << "\nPlease enter a string: ";
+    cin >> str;
+    
+for(char c : str){
+    cout << c << endl;
+    }
     return 0;
     }
